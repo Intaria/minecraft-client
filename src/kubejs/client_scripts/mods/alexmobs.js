e8f66a2bca986e76927bf8f8cf3627f9a9bc0e39
Maxim Kuznetsov <me@kaminari.pro>
2022-04-30 02:53:11 +0500

    1.18.2 update

--- src/kubejs/client_scripts/mods/alexmobs.js
+++ src/kubejs/client_scripts/mods/alexmobs.js
@@ -1,6 +1,10 @@
 //МБ в будущем выпилить что-то еще
 onEvent('jei.hide.items', event => {
     event.hide([
-        'alexmobs:animal_dictionary'
+        'alexsmobs:animal_dictionary',
+        'alexsmobs:bear_fur',
+        'alexsmobs:bone_serpent_tooth',
+        'alexsmobs:centipede_leg',
+        'alexsmobs:centipede_leggings'
     ]);
 });
