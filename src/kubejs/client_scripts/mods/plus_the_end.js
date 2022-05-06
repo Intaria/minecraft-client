5656fb37c3a447bcd7c5235707294d9533989a68
Maxim Kuznetsov <me@kaminari.pro>
2022-05-07 02:37:58 +0500

    mods updates

---
+++ src/kubejs/client_scripts/mods/plus_the_end.js
@@ -0,0 +1,13 @@
+onEvent('jei.hide.items', event => {
+    event.hide([
+        'plus_the_end:endronium_sword',
+        'plus_the_end:endronium_pickaxe',
+        'plus_the_end:endronium_shovel',
+        'plus_the_end:endronium_axe',
+        'plus_the_end:endronium_hoe',
+        'plus_the_end:endronium_helmet',
+        'plus_the_end:endronium_chestplate',
+        'plus_the_end:endronium_leggings',
+        'plus_the_end:endronium_boots',
+    ]);
+})
