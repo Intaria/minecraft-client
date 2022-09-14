e68b92724668fd4b8f6ca6a6561d0369cdd9f170
Maxim Kuznetsov <me@kaminari.pro>
2022-09-14 07:16:03 +0500

    savanna update

---
+++ src/kubejs/client_scripts/mods/cataclysm.js
@@ -0,0 +1,30 @@
+onEvent('jei.hide.items', event => {
+    event.hide([
+        //TODO: эта группа убрана НА ВРЕМЯ, пока в моде все эти штуки доступны только через креатив
+        'cataclysm:enderite_block',
+        'cataclysm:witherite_block',
+        'cataclysm:ignitium_block',
+        'cataclysm:witherite_ingot',
+        'cataclysm:enderite_ingot',
+        'cataclysm:ignitium_ingot',
+        'cataclysm:bulwark_of_the_flame',
+        'cataclysm:final_fractal',
+        'cataclysm:zweiender',
+        'cataclysm:ignitium_helmet',
+        'cataclysm:ignitium_chestplate',
+        'cataclysm:ignitium_leggings',
+        'cataclysm:ignitium_boots',
+
+        'cataclysm:infernal_forge',
+        'cataclysm:monstrous_helm',
+        'cataclysm:void_core',
+        'cataclysm:gauntlet_of_guard',
+
+        'cataclysm:ender_golem_spawn_egg',
+        'cataclysm:netherite_monstrosity_spawn_egg',
+        'cataclysm:nameless_sorcerer_spawn_egg',
+        'cataclysm:ender_guardian_spawn_egg',
+        'cataclysm:endermaptera_spawn_egg',
+        'cataclysm:ignis_spawn_egg',
+    ])
+});
